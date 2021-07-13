beff25194ee34946e18a418147ae1ebc51f94abc
Javier Zarate <zarate.javier.jz@gmail.com>
2021-07-12 20:35:29 -0600

    drop down country done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
 import './App.css';
+import { MenuItem, FormControl, Select } from '@material-ui/core';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState('worldwide');
+
+  // https://disease.sh/v3/covid-19/countries
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch('https://disease.sh/v3/covid-19/countries')
+        .then((respone) => respone.json())
+        .then((data) => {
+          // console.log('*************', data);
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  }, []);
+
+  const onCountryChange = (e) => {
+    const countryCode = e.target.value;
+    console.log('Country code ', countryCode);
+
+    setCountry(countryCode);
+  };
+
   return (
     <div className="app">
-      <h1>Let's build a covid app</h1>
+      <div className="app__header">
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value={country} onChange={onCountryChange}>
+            <MenuItem value="worldwide">WorldWide</MenuItem>
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
       {/* Header */}
       {/* Title + Select input dropdown field */}
 
