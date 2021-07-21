f5ed45c44373c4413c54d23e3c3c3ad5d5c595ef
Javier Zarate <zarate.javier.jz@gmail.com>
2021-07-20 19:54:24 -0600

    fetched historical data for my line graph

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import InfoBox from './InfoBox';
 import Map from './Map';
 import Table from './Table';
 import { sortData } from './util';
+import LineGraph from './LineGraph';
 
 function App() {
   const [countries, setCountries] = useState([]);
@@ -114,7 +115,7 @@ function App() {
           <Table countries={tableData} />
           <h3>World wide new cases</h3>
         </CardContent>
-        {/* table */}
+        <LineGraph />
         {/* graph */}
       </Card>
     </div>
--- src/LineGraph.js
+++ src/LineGraph.js
@@ -1,11 +1,34 @@
-import React from 'react';
-import { line } from 'react-chartjs-2';
+import React, { useEffect, useState } from 'react';
+// import { Line } from 'react-chartjs-2';
 
 function LineGraph() {
-  return;
-  <div>
-    <Line data options />
-  </div>;
+  const [data, setData] = useState({});
+  // https://disease.sh/v3/covid-19/historical/all?lastdays=120
+
+  useEffect(() => {
+    fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }, []);
+
+  // useEffect(() => {
+  //     fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
+  //       .then((response) => {
+  //         return response.json();
+  //       })
+  //       .then((data) => {
+  //         console.log(data)
+  //       });
+  // }, []);
+
+  return (
+    <div>
+      <h1>I AM A GRAPH</h1>
+      {/* <Line data options /> */}
+    </div>
+  );
 }
 
 export default LineGraph;
